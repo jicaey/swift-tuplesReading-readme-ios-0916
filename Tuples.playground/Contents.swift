988842aa8c17c3457c2d39792fd87cae67b6b3de
jicaey <young.ma@me.com>
2016-09-07 16:27:19 -0400

    Play

--- Tuples.playground/Contents.swift
+++ Tuples.playground/Contents.swift
@@ -17,6 +17,7 @@ person.age
 person.name
 print("\(person.name)'s age is \(person.age)")
 
+
 var person2 = (name: "Jim", age: 30)
 print("\(person2.name)'s age is \(person2.age)")
 person2.age = 32
